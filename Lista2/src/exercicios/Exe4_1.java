7d129cac9cf0dae4dfe9ec7e99c3ab85b1ba8000
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-01-27 13:03:44 -0300

    Create Exe4_1.java

---
+++ Lista2/src/exercicios/Exe4_1.java
@@ -0,0 +1,26 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exe4_1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Insira um valor: ");
+		int numero = scanner.nextInt();
+		
+		if(numero % 2 == 0) 
+		{
+			System.out.printf("\nO número %d é par\n", numero);
+			System.out.printf("A raiz quadrada de %d é %.2f\n", numero, Math.sqrt(numero));
+		}
+		else 
+		{
+			System.out.printf("\nO número %d é ímpar\n", numero);
+			System.out.printf("O número %d ao quadrado vale %d", numero, (int) Math.pow(numero, 2));
+		}
+		
+		
+		scanner.close();
+	}
+}
