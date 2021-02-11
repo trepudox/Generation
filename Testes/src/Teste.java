55f7f61d1865f8c08fd87d608bdf89195abf11e5
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-02-10 22:20:12 -0300

    teste

--- Testes/src/Teste.java
+++ Testes/src/Teste.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Teste {
 
@@ -8,7 +9,26 @@ public class Teste {
 //		lista.add(11);
 //		
 //		System.out.println();
+		Scanner scanner = new Scanner(System.in);
 		
+		for(int i = 0; i < 3; i++) 
+		{
+			System.out.println("oi");
+		}
+		
+		int entrada = 0;
+		System.out.println("\n\n\n");
+		
+		do
+		{
+			System.out.println("oi");
+			System.out.print("Digite o valor de entrada: ");
+			entrada = scanner.nextInt();
+
+		} while(entrada != 0);
+	
+		
+		System.out.println("fim");
 	}
 	
 }
