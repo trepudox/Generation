48c6b5caa9365b5603dff63138cdae713b5823a4
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-03-02 14:43:04 -0300

    model e repository criados

---
+++ Java/blogPessoal/src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -0,0 +1,77 @@
+package org.generation.blogPessoal.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(min = 2, max = 50, message = "Mínimo de 2 caracteres e máximo de 50")
+	private String nome;
+	
+	@NotNull
+	@Size(min = 2, max = 500, message = "Mínimo de 2 caracteres e máximo de 500")
+	private String descricao;
+	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagens;
+
+	@Deprecated
+	public Tema() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Postagem> getPostagens() {
+		return postagens;
+	}
+
+	public void setPostagens(List<Postagem> postagens) {
+		this.postagens = postagens;
+	}
+
+	@Override
+	public String toString() {
+		return "Tema [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", postagens=" + postagens + "]";
+	}
+	
+}
---
+++ Java/blogPessoal/src/main/java/org/generation/blogPessoal/repository/TemaRepository.java
@@ -0,0 +1,10 @@
+package org.generation.blogPessoal.repository;
+
+import org.generation.blogPessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	
+}
